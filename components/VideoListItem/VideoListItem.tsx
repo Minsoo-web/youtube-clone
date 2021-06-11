0d0792801d5a8b569b509af82dd40439d570f048
minsoo kim <zlemzlem5656@naver.com>
2021-06-12 04:14:29 +0900

    [수정] video 컴포넌트 마무리

--- components/VideoListItem/VideoListItem.tsx
+++ components/VideoListItem/VideoListItem.tsx
@@ -21,6 +21,23 @@ type VideoListItemProps = {
 
 const VidoeListItem = (props: VideoListItemProps) => {
   const { video } = props;
+
+  const minutes: number = Math.floor(video.duration / 60);
+  const seconds: number = video.duration % 60;
+  const computedSeconds: string = seconds < 10 ? "0" : "" + seconds;
+
+  let viewString: any = video.views;
+
+  if (video.views > 1_000_100) {
+    viewString = video.views / 1_000_000;
+    if (Number.isInteger(viewString)) viewString = viewString + "m";
+    else viewString = viewString.toFixed(1) + "m";
+  } else if (video.views > 1_000) {
+    viewString = video.views / 1_000;
+    if (Number.isInteger(viewString)) viewString = viewString + "k";
+    else viewString = viewString.toFixed(1) + "k";
+  }
+
   return (
     <View>
       {/* Thmbnail */}
@@ -32,7 +49,9 @@ const VidoeListItem = (props: VideoListItemProps) => {
           }}
         />
         <View style={styles.timeContainer}>
-          <Text style={styles.time}>{video.duration}</Text>
+          <Text style={styles.time}>
+            {minutes}:{computedSeconds}
+          </Text>
         </View>
       </View>
       {/* Title row */}
@@ -49,7 +68,7 @@ const VidoeListItem = (props: VideoListItemProps) => {
           <Text style={styles.title}>{video.title}</Text>
           <Text
             style={styles.subtitle}
-          >{`${video.user.name} ${video.views} ${video.createdAt}`}</Text>
+          >{`${video.user.name} ${viewString} ${video.createdAt}`}</Text>
         </View>
         {/* Icon */}
         <Entypo name="dots-three-vertical" size={18} color="white" />
